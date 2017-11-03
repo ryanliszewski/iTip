f39158a531a8014e9c0e513cce2db83e1b89ce7a
Ryanliszewski <ryanliszewski@gmail.com>
2017-11-03 16:44:47 -0700

    Removed design skeleton. Started animations but are buggy.

--- iTip/iTip/HomeViewController.swift
+++ iTip/iTip/HomeViewController.swift
@@ -10,12 +10,48 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
-	@IBOutlet weak var billAmountTextField: UITextField!
-	
-    override func viewDidLoad() {
-        super.viewDidLoad()
-		billAmountTextField.setBottomBorder()
+  @IBOutlet weak var billAmountViewHeight: NSLayoutConstraint!
+  @IBOutlet weak var billAmountTextField: UITextField!
+  @IBOutlet weak var billAmountView: UIView!
+  var billAmountViewCenter: CGPoint!
+  var billAmountViewY: CGFloat!
+  var heightKeyboard: CGFloat! {
+    didSet {
+       billAmountViewHeight.constant = self.view.frame.height - heightKeyboard
     }
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    billAmountTextField.becomeFirstResponder()
+    NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardShown(notification:)), name: NSNotification.Name.UIKeyboardDidShow, object: nil)
+    billAmountTextField.delegate = self
+  }
+  
+  func keyboardShown(notification: NSNotification) {
+    if let infoKey  = notification.userInfo?[UIKeyboardFrameEndUserInfoKey],
+      let rawFrame = (infoKey as AnyObject).cgRectValue {
+      let keyboardFrame = view.convert(rawFrame, from: nil)
+      self.heightKeyboard = keyboardFrame.size.height
+    }
+  }
+  
+}
+
+//MARK: - UITextFieldDelegate
+extension HomeViewController: UITextFieldDelegate {
+  public func textFieldDidBeginEditing(_ textField: UITextField){
+    UIView.animate(withDuration: 0.4) {
+      self.billAmountViewHeight.constant = self.billAmountViewHeight.constant / 2.0
+
+    }
+  }
+  
+  public func textFieldDidEndEditing(_ textField: UITextField) {
+    UIView.animate(withDuration: 0.4) {
+      self.billAmountView.center = CGPoint(x: self.billAmountView.center.x, y: self.billAmountView.center.y * 2)
+    }
+  }
 }
 
 //MARK: - UITextField Extension
@@ -31,3 +67,4 @@ extension UITextField {
 		self.layer.shadowRadius = 0.0
 	}
 }
+
