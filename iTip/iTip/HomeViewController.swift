abe385d1ae634e54ef36ec49e573b764d8dd2e19
Ryanliszewski <ryanliszewski@gmail.com>
2017-11-03 18:22:30 -0700

    Updated UI. Basic design is done.

--- iTip/iTip/HomeViewController.swift
+++ iTip/iTip/HomeViewController.swift
@@ -10,22 +10,26 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
-  @IBOutlet weak var billAmountViewHeight: NSLayoutConstraint!
+
+
+
+  @IBOutlet weak var tipSegmentedController: UISegmentedControl!
   @IBOutlet weak var billAmountTextField: UITextField!
   @IBOutlet weak var billAmountView: UIView!
   var billAmountViewCenter: CGPoint!
   var billAmountViewY: CGFloat!
   var heightKeyboard: CGFloat! {
     didSet {
-       billAmountViewHeight.constant = self.view.frame.height - heightKeyboard
     }
   }
-
+  @IBOutlet weak var enterBillAmountLabel: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    billAmountTextField.becomeFirstResponder()
     NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardShown(notification:)), name: NSNotification.Name.UIKeyboardDidShow, object: nil)
     billAmountTextField.delegate = self
+    //billAmountTextField.setBottomBorder()
+    
   }
   
   func keyboardShown(notification: NSNotification) {
@@ -41,9 +45,18 @@ class HomeViewController: UIViewController {
 //MARK: - UITextFieldDelegate
 extension HomeViewController: UITextFieldDelegate {
   public func textFieldDidBeginEditing(_ textField: UITextField){
-    UIView.animate(withDuration: 0.4) {
-      self.billAmountViewHeight.constant = self.billAmountViewHeight.constant / 2.0
+    
+    if(textField.text != nil){
+      UIView.beginAnimations(nil, context: nil)
+      UIView.setAnimationDuration(0.3)
+      enterBillAmountLabel.alpha = 0
+      self.billAmountView.center = CGPoint(x: self.billAmountView.center.x, y: self.billAmountView.center.y / 1.2)
+      
+     
+      self.billAmountTextField.setSelectedBottomBorder()
 
+      
+      UIView.commitAnimations()
     }
   }
   
@@ -58,13 +71,21 @@ extension HomeViewController: UITextFieldDelegate {
 extension UITextField {
 	func setBottomBorder(){
 		self.borderStyle = .none
-		self.layer.backgroundColor = UIColor.white.cgColor
-		
+    self.backgroundColor = #colorLiteral(red: 0.4692698717, green: 0.6561034322, blue: 0.4752988815, alpha: 1)
 		self.layer.masksToBounds = false
 		self.layer.shadowColor = UIColor.gray.cgColor
-		self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+		self.layer.shadowOffset = CGSize(width: 1.0, height: 2.0)
 		self.layer.shadowOpacity = 1.0
 		self.layer.shadowRadius = 0.0
 	}
+  func setSelectedBottomBorder(){
+    self.borderStyle = .none
+    self.backgroundColor = #colorLiteral(red: 0.4692698717, green: 0.6561034322, blue: 0.4752988815, alpha: 1)
+    self.layer.masksToBounds = false
+    self.layer.shadowOffset = CGSize(width: 2.0, height: 2.0)
+    self.layer.shadowColor = UIColor.white.cgColor
+    self.layer.shadowOpacity = 1.0
+    self.layer.shadowRadius = 1.0
+  }
 }
 
